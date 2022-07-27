1bea9bb31a295401878c1e574ec0185541fd70b0
ari <ariw2912@gmail.com>
2022-07-27 19:35:20 +0300

    add big-char

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template, template_rendered
+from flask import Flask, render_template, request, flash
+from big-app import *
+import psycopg2
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
 
-app.run(host='127.0.0.1', port=5003)
+@app.route("/")
+def index():
+	flash("what's your name?")
+	return render_template("index.html")
+
+@app.route("greet", methods=["POST", "GET"])
+def greet():
+    print("greet")
+    str = request.form['name_input']
+    ans = big_char_app.main(str)
+    # flash(ans)
+    # # request.form['name_input']
+    ans = '<html><head><title>'+str+'</title></head><body><p><pre>' + ans + '</pre></p></body></html>'
+    outfile = open('./templates/anser.html', 'w')
+    outfile.write(ans)
+    outfile.close()
+    f = open('./templates/anser.html', 'r')
+    # return render_template("index.html")
+    return render_template("ans.html")
+
+app.run(host='127.0.0.1', port=5005)
