b04056f6c3677ba36084b7ea24f8088f783ab70b
ari <ariw2912@gmail.com>
2022-07-27 19:09:10 +0300

    start development web app

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, template_rendered
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+app.run(host='127.0.0.1', port=5003)
